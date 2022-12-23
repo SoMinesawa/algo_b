e2ff3c3fd6d19b431f622636fa87e6e81ac6af8c
KohsukeIde <kohsukeide0726@gmail.com>
2022-12-23 16:20:43 +0900

    Change: changes on calc_edit_distance function

--- sample/adsb_G1.c
+++ sample/adsb_G1.c
@@ -12,38 +12,23 @@ int min(int a, int b, int c) {
     return min;
 }
 
-int calc_edit_distance(const char* str1, const char* str2) {
-    // str1の文字数をn、str2の文字数をmとする
-    int n = strlen(str1);
-    int m = strlen(str2);
+int calc_edit_distance(char *str1, char *str2) {
+    int lenstr1 = strlen(str1) + 1;
+    int lenstr2 = strlen(str2) + 1;
+    int d[lenstr1][lenstr2];
+    int i1 = 0, i2 = 0, cost = 0;
 
-    // 配列dを宣言する
-    // d[i][j]は、str1のi文字目から末尾までと、str2のj文字目から末尾までの
-    // 編集距離を表す
-    int d[n + 1][m + 1];
+    for (; i1 < lenstr1; i1++) d[i1][0] = i1;
+    for (; i2 < lenstr2; i2++) d[0][i2] = i2;
 
-    // 配列dを初期化する
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= m; j++) {
-            if (i == 0) {
-                // str1が空文字列の場合、str2をすべて挿入する必要がある
-                d[i][j] = j;
-            } else if (j == 0) {
-                // str2が空文字列の場合、str1をすべて削除する必要がある
-                d[i][j] = i;
-            } else {
-                // str1とstr2の末尾の文字が等しい場合、
-                // str1のi-1文字目から末尾までと、str2のj-1文字目から末尾までの
-                // 編集距離に1を足さずに、そのままd[i][j]とする
-                // 等しくない場合、str1の末尾の文字をstr2の末尾の文字で置き換える必要がある
-                d[i][j] =
-                    min(d[i - 1][j] + 1, d[i][j - 1] + 1,
-                        d[i - 1][j - 1] + str1[i - 1] == str2[j - 1] ? 0 : 1);
-            }
+    for (i1 = 1; i1 < lenstr1; i1++) {
+        for (i2 = 1; i2 < lenstr2; i2++) {
+            cost = str1[i1 - 1] == str2[i2 - 1] ? 0 : 1;
+            d[i1][i2] = min(d[i1 - 1][i2] + 1, d[i1][i2 - 1] + 1,
+                            d[i1 - 1][i2 - 1] + cost);
         }
     }
-    // 編集距離を返す
-    return d[n][m];
+    return d[lenstr1 - 1][lenstr2 - 1];
 }
 
 int main(int argc, char* argv[]) {
@@ -68,6 +53,8 @@ int main(int argc, char* argv[]) {
         fscanf(input_file, "%s", S[i]);
     }
 
+
+
     for(int i = 0; i < Q; i++) {
         char* q = (char*)malloc(sizeof(char) * 200);
         fscanf(input_file, "%s", q);
@@ -77,13 +64,20 @@ int main(int argc, char* argv[]) {
         int min_edit_distance = INT_MAX;
         int min_edit_distance_index = 0;
         for(int j = 0; j < N; j++) {
-            int edit_distance = calc_edit_distance(q, S[j]);
-            if(edit_distance < min_edit_distance) {
+            char tmp[101];
+            for(int i = 0; i < 50; i++){
+              strncpy(tmp, &S[j][100*i], 101);
+              int edit_distance = calc_edit_distance(q, tmp);
+              if(edit_distance < min_edit_distance) {
                 min_edit_distance = edit_distance;
                 min_edit_distance_index = j;
+              }
             }
+            
+            
         }
         fprintf(output_file, "%d\n", min_edit_distance_index + 1);
+        
     }
     fclose(input_file);
     fclose(output_file);
